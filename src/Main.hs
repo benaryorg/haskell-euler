a5b52e4feeebe12153d612181735785c98fe8f3b
benaryorg <binary@benary.org>
2016-10-13 01:34:00 +0200

    add: 1
    Signed-off-by: benaryorg <binary@benary.org>

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,17 @@ module Main where
 import Control.Monad
 import System.Environment
 
+applyAll :: [a -> b] -> ([b] -> c) -> a -> c
+applyAll mapper folder = folder . zipWith ($) mapper . replicate (length mapper)
+
+divisibleBy :: Integral a => ((a -> Bool) -> [a] -> Bool) -> [a] -> a -> Bool
+divisibleBy foo numbers = applyAll (map (flip mod) numbers) (foo (==0))
+
 functionList :: [(Int,IO String)]
 functionList =
 	[
 		(0,return "Meow.")
+		, (1,return $ show $ sum $ filter (divisibleBy any [3,5]) [1..999])
 	]
 
 main :: IO ()
