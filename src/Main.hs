5af3d91c126912b6aaffae10de71e408c98d35c6
benaryorg <binary@benary.org>
2016-10-12 20:42:25 +0200

    basic construct
    Signed-off-by: benaryorg <binary@benary.org>

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,18 @@
 module Main where
 
+import Control.Monad
+import System.Environment
+
+functionList :: [(Int,IO String)]
+functionList =
+	[
+		(0,return "Meow.")
+	]
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+	args <- getArgs 
+	case lookup (read $ args!!0) functionList of
+		Nothing -> putStrLn "Number not in list"
+		Just foo -> join $ (fmap (putStrLn)) foo
+
