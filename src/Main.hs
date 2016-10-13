68e193d8070203be3660e02557a7927c0b287e9b
benaryorg <binary@benary.org>
2016-10-13 02:14:01 +0200

    add: 3
    Signed-off-by: benaryorg <binary@benary.org>

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,11 @@ import System.Environment
 fibonacci :: Integral a => [a]
 fibonacci = unfoldr (\(x,y) -> Just (x,(y,x+y))) (1,1)
 
+primeFactor :: Integral a => a -> [a]
+primeFactor 1 = []
+primeFactor x = f:(primeFactor (x `div` f))
+	where f = head $ filter ((==0) . mod x) $ (2:[3,5..])
+
 applyAll :: [a -> b] -> ([b] -> c) -> a -> c
 applyAll mapper folder = folder . zipWith ($) mapper . replicate (length mapper)
 
@@ -19,6 +24,7 @@ functionList =
 		(0,return "Meow.")
 		, (1,return $ show $ sum $ filter (divisibleBy any [3,5]) [1..999])
 		, (2,return $ show $ sum $ filter odd $ takeWhile (<= 4000000) fibonacci)
+		, (3,return $ show $ last $ primeFactor 600851475143)
 	]
 
 main :: IO ()
