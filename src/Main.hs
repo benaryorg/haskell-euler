45b1a19a9ae93cb40b3558b16728816e6087d728
benaryorg <binary@benary.org>
2016-10-13 02:28:35 +0200

    add: 4
    Signed-off-by: benaryorg <binary@benary.org>

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,26 @@ primeFactor 1 = []
 primeFactor x = f:(primeFactor (x `div` f))
 	where f = head $ filter ((==0) . mod x) $ (2:[3,5..])
 
+isPalindrom :: Integral a => a -> a -> Bool
+isPalindrom base number = (take len dig) == (take len (reverse dig))
+	where
+		dig = digits base number
+		len = div (length dig) 2
+
+isPalindromBin = isPalindrom 2
+isPalindromOct = isPalindrom 8
+isPalindromDec = isPalindrom 10
+isPalindromHex = isPalindrom 16
+
+digits :: Integral a => a -> a -> [a]
+digits base 0 = []
+digits base number = (number `mod` base):(digits base $ number `div` base)
+
+digitsBin = digits 2
+digitsOct = digits 8
+digitsDec = digits 10
+digitsHex = digits 16
+
 applyAll :: [a -> b] -> ([b] -> c) -> a -> c
 applyAll mapper folder = folder . zipWith ($) mapper . replicate (length mapper)
 
@@ -25,6 +45,7 @@ functionList =
 		, (1,return $ show $ sum $ filter (divisibleBy any [3,5]) [1..999])
 		, (2,return $ show $ sum $ filter odd $ takeWhile (<= 4000000) fibonacci)
 		, (3,return $ show $ last $ primeFactor 600851475143)
+		, (4,return $ show $ maximum $ filter isPalindromDec $ concat $ [[i*j|i <- [999,998..100]]|j <- [999,998..100]])
 	]
 
 main :: IO ()
